9a7012811ff7330ddf066619d9ee4f20ae3100ac
PP-ONTIME\Ismat_r <Ismat_r@pp-ontime.co.th>
2023-06-21 17:18:28 +0700

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const cron = require("node-cron");
 const nodemailer = require("nodemailer");
+
 const express = require("express");
 let bodyParser = require("body-parser");
+
 const request = require("request");
+
 const dotenv = require("dotenv");
 dotenv.config();
+
+const line = require("@line/bot-sdk");
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
+};
+const client = new line.Client(config);
 
 const app = express();
 app.use(bodyParser.json());
@@ -29,42 +39,39 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/webhook", (req, res) => res.sendStatus(200));
+// app.post("/webhook", (req, res) => res.sendStatus(200));
 
 // app.post("/webhook", (req, res) => {
-//   let reply_token = req.body.events[0].replyToken;
-//   reply(reply_token);
+//   // let reply_token = req.body.events[0].replyToken;
+//   // reply(reply_token);
+//   const client = new line.Client({
+//     channelAccessToken: "<channel access token>",
+//   });
 //   res.sendStatus(200);
 // });
 
-function reply(reply_token) {
-  let headers = {
-    "Content-Type": "application/json",
-    Authorization: "Bearer {5f6515b44dc937512f5251cc835d134c}",
-  };
-  let body = JSON.stringify({
-    replyToken: reply_token,
-    messages: [
-      {
-        type: "text",
-        text: "Hello",
-      },
-      {
-        type: "text",
-        text: "How are you?",
-      },
-    ],
-  });
-  request.post(
-    {
-      url: "https://api.line.me/v2/bot/message/reply",
-      headers: headers,
-      body: body,
-    },
-    (err, res, body) => {
-      console.log("status = " + res.statusCode);
-    }
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
   );
+});
+
+function handleEvent(event) {
+  console.log(event);
+  if (event.type === "message" && event.message.type === "text") {
+    handleMessageEvent(event);
+  } else {
+    return Promise.resolve(null);
+  }
+}
+
+function handleMessageEvent(event) {
+  var msg = {
+    type: "text",
+    text: "สวัสดีครัช",
+  };
+
+  return client.replyMessage(event.replyToken, msg);
 }
 
 //Email schedules code comes here
